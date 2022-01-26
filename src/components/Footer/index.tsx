d9f27ab6c4264c0b1c0a06fad45744f08009cfc3
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-01-26 09:04:51 -0300

    Atualizando o href do linkedin

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
     <ContainerStyled>
       <IconsStyled>
         <a href="https://github.com/arthurfortunato" target="_blank" rel="noreferrer"><FiGithub size={"20px"}/></a>
-        <a href="https://www.linkedin.com/in/arthur-forturnato-643828187/" target="_blank" rel="noreferrer" ><FiLinkedin size={"20px"}/></a>
+        <a href="https://www.linkedin.com/in/arthur-fortunato-%F0%9F%92%BB-643828187/" target="_blank" rel="noreferrer" ><FiLinkedin size={"20px"}/></a>
         <a href="https://www.instagram.com/luizarthur.fortunato/" target="_blank" rel="noreferrer"><FiInstagram size={"20px"}/></a>
       </IconsStyled>
 
